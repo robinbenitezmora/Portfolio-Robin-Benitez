3eee97ea1e96bf8be003a83df12431d24351f6ac
robinbenitezmora <robinbenitez1990@gmail.com>
2022-07-21 15:52:47 -0500

    Create functions in main.js file in order to validate the formulary

--- main.js
+++ main.js
@@ -84,3 +84,65 @@ const closeModal = () => {
 
 openModal();
 closeModal();
+
+// Validation of the form
+const nameError = document.getElementById('name-error');
+const emailError = document.getElementById('email-error');
+const messageError = document.getElementById('message-error');
+const submitError = document.getElementById('submit-error');
+
+function validateName () {
+  const name = document.getElementById('name');
+  if (name.value.length === 0) {
+    nameError.innerText = 'Your name is required';
+    return false;
+  } if (!name.value.match(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/)) {
+    nameError.innerText = 'Write your name correctly';
+    return false;
+  }
+  nameError.innerHTML = '<span class="fa-solid fa-circle-check"></span>';
+  return true;
+}
+
+function validateEmail () {
+  const email = document.getElementById('email');
+  if (email.value.length === 0) {
+    emailError.innerText = 'Your email is required';
+    return false;
+  } if (!email.value.match(/^[a-z._\-[0-9]*[@][a-z]*\.[a-z]{2,4}$/)) {
+    emailError.innerText = 'Write your email correctly';
+    return false;
+  }
+  emailError.innerHTML = '<span class="fa-solid fa-circle-check"></span>';
+  return true;
+}
+
+function validateMessage () {
+  const message = document.getElementById('message-area');
+  if (message.value.length === 0) {
+    messageError.innerText = 'Your message is required';
+    return false;
+  } if (message.value.length < 5 || message.value.length > 500) {
+    messageError.innerText = 'Write your message between 5 and 500 characters';
+    return false;
+  }
+  messageError.innerHTML = '<span class="fa-solid fa-circle-check"></span>';
+  return true;
+}
+
+function validateForm () {
+  if (validateName() && validateEmail()) {
+    submitError.style.display = 'none';
+    return true;
+  }
+  submitError.style.display = 'block';
+  submitError.innerText = 'Please fill out the form correctly';
+  setTimeout(function() { submitError.style.display = 'none'; }, 3000);
+  return false;
+}
+
+function validateFormSubmit () {
+  if (validateForm()) {
+    document.getElementById('form').submit();
+  }
+}
