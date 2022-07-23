47fa2b660ed434768d6f7309f42efa745c519501
robinbenitezmora <robinbenitez1990@gmail.com>
2022-07-22 21:48:21 -0500

    Creat a bug inside Javascript code lines

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const openSlideMenu = document.querySelector('.menu');
 const closeSlideMenu = document.querySelector('.cls');
-const closeNav = document.querySelector('.items');
+
 
 openSlideMenu.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.add('active');
