c437725aa6efe56b9e9ab2a99c87f52bbf7923e7
robinbenitezmora <robinbenitez1990@gmail.com>
2022-07-23 09:29:09 -0500

    Correct linters error in javascript bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const openSlideMenu = document.querySelector('.menu');
 const closeSlideMenu = document.querySelector('.cls');
-const closeNav = 'closeNav';
+const closeNav = document.querySelector('.items');
 
 openSlideMenu.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.add('active');
